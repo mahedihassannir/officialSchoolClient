23ee10aedb59847838b603b90a0e742bab640277
mahedi6666655555 <mahedinir34678@gmail.com>
2023-06-13 00:39:13 +0600

    notification system and cart in the nav is done

--- src/Pages/Food.jsx
+++ src/Pages/Food.jsx
@@ -7,12 +7,12 @@ const Food = () => {
 
 
 
-    return (
+    return (g
         <div>
 
             <h1 className="text-[#db87f0] text-4xl text-center font-bold mb-10">আমাদের স্কুলের খাবার</h1>
 
-            <div className="my-10 grid grid-cols-5 gap-2">
+            <div className="my-10 grid sm:grid-cols-1 md:grid-cols-2  lg:grid-cols-5  gap-2">
                 {
                     data.map(res => <FoodSingle key={res._id} data={res}></FoodSingle>)
                 }
--- src/Shared/Nav.jsx
+++ src/Shared/Nav.jsx
@@ -27,6 +27,9 @@ const Nav = () => {
 
 
 
+
+
+
     return (
         <div >
 
@@ -43,20 +46,49 @@ const Nav = () => {
                 </Link>
                 <ul className="flex gap-4 items-center">
                     <Link className="hover:text-[#340e8d]" >Home</Link>
-                    <Link className="hover:text-[#340e8d]" >Blog</Link>
+                    <Link className="hover:text-[#340e8d] " >Blog</Link>
                     <Link className="hover:text-[#340e8d]" >Register in school</Link>
                     <Link className="hover:text-[#340e8d]" to='/OurTeachers'>OurTeachers</Link>
                     <Link className="hover:text-[#340e8d]" to="OurTeachers">Curses</Link>
                     <Link className="hover:text-[#340e8d]" to="OurTeachers">my classes</Link>
                     <Link className=" hover:text-[#340e8d] btn">enroll now</Link>
 
-                    <div className=" cursor-pointer indicator">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" /></svg>
+
+
+                    {/* here is the cart icon */}
+                    <div className="flex-none">
+                        <div className="dropdown dropdown-end">
+                            <label tabIndex={0} className="btn btn-ghost btn-circle">
+                                <div className="indicator">
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
+                                    <span className="badge badge-sm indicator-item">{cart.length || 0}</span>
+                                </div>
+                            </label>
+                            <div tabIndex={0} className="mt-3 card card-compact dropdown-content w-52 bg-base-100 shadow">
+                                <div className="card-body">
+                                    <span className="font-bold text-lg">{cart.length || 0} Items</span>
+                                    <span className="text-info">Subtotal: $999</span>
+                                    <Link to="/cart" className="card-actions">
+                                        <button className="btn btn-primary btn-block">View cart</button>
+                                    </Link>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+
+                    {/* ends */}
+
+                    {/* thsis is notify section */}
+                    <div className=" cursor-pointer indicator ">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="  h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" /></svg>
                         {
-                            cart ? < span className="badge badge-xs badge-primary indicator-item"></span> :
+                            cart ? < span className="badge badge-xs badge-primary  indicator-item"></span> :
                                 <></>
                         }
                     </div>
+
+                    {/* ends */}
 
                     {/* this is dropdown pofile */}
 
